89342996a3da647b43d38bddae741079876b32a8
AkechiAce <GWSBrooks-Prenger01-ICS2O1-01@hdsb.ca>
2019-11-20 10:27:12 -0500

    Finished Drive Code using TankDrive to use joystick values to determine speed.

--- Vimy/src/main/java/org/usfirst/frc5409/Vimy/OI.java
+++ Vimy/src/main/java/org/usfirst/frc5409/Vimy/OI.java
@@ -12,6 +12,10 @@
 package org.usfirst.frc5409.Vimy;
 
 import org.usfirst.frc5409.Vimy.commands.*;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
