d9ff5d22677915fcfa0bc270c15f5c391e88a33d
James Tang <1tangjyt@gmail.com>
2019-11-26 16:46:51 -0500

    fixed brakets (merci mr. brooks)

--- Vimy/src/main/java/org/usfirst/frc5409/Vimy/OI.java
+++ Vimy/src/main/java/org/usfirst/frc5409/Vimy/OI.java
@@ -1,24 +1,61 @@
 package org.usfirst.frc5409.Vimy;
 
-import com.sun.org.apache.xerces.internal.impl.xpath.XPath.Axis;
-
 import org.usfirst.frc5409.Vimy.commands.*;
-
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.usfirst.frc5409.Vimy.subsystems.*;
 
 public class OI {
+    public JoystickButton joystickButton1;
+    public JoystickButton joystickButton2;
+    public JoystickButton joystickButton3;
+    public JoystickButton joystickButton4;
+    public JoystickButton joystickButton5;
+    public Joystick driveStick;
+    public JoystickButton joystickButton6;
+    public JoystickButton joystickButton7;
+    public JoystickButton joystickButton8;
+    public JoystickButton joystickButton9;
+    public Joystick controlStick;
     
     public OI() {
-    XboxController driverController = new XboxController(0);
-    public Joystick getControlStick(){
-        return controlStick;
+        controlStick = new Joystick(1);
+        
+        joystickButton9 = new JoystickButton(controlStick, 4);
+        joystickButton9.whileHeld(new AutonomousCommand());
+        joystickButton8 = new JoystickButton(controlStick, 3);
+        joystickButton8.whileHeld(new AutonomousCommand());
+        joystickButton7 = new JoystickButton(controlStick, 2);
+        joystickButton7.whileHeld(new AutonomousCommand());
+        joystickButton6 = new JoystickButton(controlStick, 1);
+        joystickButton6.whileHeld(new AutonomousCommand());
+        driveStick = new Joystick(0);
+        
+        joystickButton5 = new JoystickButton(driveStick, 5);
+        joystickButton5.whileHeld(new AutonomousCommand());
+        joystickButton4 = new JoystickButton(driveStick, 4);
+        joystickButton4.whileHeld(new AutonomousCommand());
+        joystickButton3 = new JoystickButton(driveStick, 3);
+        joystickButton3.whileHeld(new AutonomousCommand());
+        joystickButton2 = new JoystickButton(driveStick, 2);
+        joystickButton2.whileHeld(new AutonomousCommand());
+        joystickButton1 = new JoystickButton(driveStick, 1);
+        joystickButton1.whileHeld(new AutonomousCommand());
+
+        SmartDashboard.putData("Autonomous Command", new AutonomousCommand());
     }
-    public double getDriverRawAxis(int axis){
-        return driverController.getRawAxis(axis);
-    }
+
+        public Joystick getdriveStick() {
+            return driveStick;
+        }
+    
+        public Joystick getcontrolStick() {
+            return controlStick;
+        }
+public double GetDriverRawAxis(int axis){
+    return driveStick.getRawAxis(axis);
     }
 }
+
 
--- Vimy/src/main/java/org/usfirst/frc5409/Vimy/commands/TankDrive.java
+++ Vimy/src/main/java/org/usfirst/frc5409/Vimy/commands/TankDrive.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package org.usfirst.frc5409.Vimy.commands;
 
 import org.usfirst.frc5409.Vimy.Robot;
@@ -21,11 +14,10 @@ public class TankDrive extends Command {
 
   }
 
-  // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    double leftStickY = Robot.oi.getDriverRawAxis(1);
-    double rightStickY = Robot.oi.getDriverRawAxis(0);
+    double leftStickY = Robot.oi.GetDriverRawAxis(0);
+    double rightStickY = Robot.oi.GetDriverRawAxis(1);
 
     Robot.driveTrain.setLeftMotors(leftStickY*0.5);
     Robot.driveTrain.setRightMotors(rightStickY*0.5);
@@ -40,8 +32,8 @@ public class TankDrive extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
-    Robot.driveTrain.setLeftMotor(0);
-    Robot.driveTrain.setRightMotor(0);
+    Robot.driveTrain.setLeftMotors(0);
+    Robot.driveTrain.setRightMotors(0);
   }
 
   // Called when another command which requires one or more of the same
