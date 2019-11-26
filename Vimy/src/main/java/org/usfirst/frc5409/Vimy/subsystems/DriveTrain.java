c410dcf5fd535aca489f86e7d80b43f83e87e260
James Tang <1tangjyt@gmail.com>
2019-11-26 16:57:15 -0500

    Rename

--- Vimy/src/main/java/org/usfirst/frc5409/Vimy/subsystems/DriveTrain.java
+++ Vimy/src/main/java/org/usfirst/frc5409/Vimy/subsystems/DriveTrain.java
@@ -76,7 +76,7 @@ public class DriveTrain extends Subsystem {
 
     @Override
     public void initDefaultCommand() {
-        setDefaultCommand(new TankDrive());
+        setDefaultCommand(new Drive());
     }
 
     @Override
