8942d3f4265d6a5707df58a4255b29c0c887b2a1
AkechiAce <GWSBrooks-Prenger01-ICS2O1-01@hdsb.ca>
2019-11-27 11:10:04 -0500

    Adjusted values to turn the robot right, this is just rough work to be used as an example for command groups.

--- Vimy/src/main/java/org/usfirst/frc5409/Vimy/commands/TurnRight.java
+++ Vimy/src/main/java/org/usfirst/frc5409/Vimy/commands/TurnRight.java
@@ -10,17 +10,14 @@ package org.usfirst.frc5409.Vimy.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class MoveSequence extends CommandGroup {
-  /**
-   * Add your docs here.
-   */
+  //Command group for a right turn that can be used in conjunction with encoders to change values for precise pathing
   public MoveSequence() {
-    //Command group tests
     //Moves forward at half speed for two seconds
     addSequential(new Move(2,0.5,0.5));
-    //Moves backward at three quarters speed for five seconds
-    addSequential(new Move(5,-0.7,-0.7));
-    //Spins for 10 seconds
-    addSequential(new Move(10, -1,1));
+    //Turn to the right at three quarters speed for five seconds
+    addSequential(new Move(1.5,-0.75, 0.75));
+    //Move forward at three quarters speed for two seconds 
+    addSequential(new Move(2, 0.75, 0.75));
     // Add Commands here:
     // e.g. addSequential(new Command1());
     // addSequential(new Command2());
