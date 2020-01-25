d4aa46760a53b2469fdd72b7f216bcc3ac7d50cd
Clarified210 <nickseplarsky210@gmail.com>
2020-01-25 11:12:45 -0500

    asd

--- Vimmydrivecode/src/main/java/frc/robot/subsystems/DriveCode.java
+++ Vimmydrivecode/src/main/java/frc/robot/subsystems/DriveCode.java
@@ -7,9 +7,16 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveCode extends SubsystemBase {
+ private TalonSRX motorLeft1 = new TalonSRX(RobotMap.MOTOR_LEFT_1_ID);
+ private TalonSRX motorLeft2 = new TalonSRX(RobotMap.MOTOR_LEFT_2_ID);
+ private TalonSRX motorRight1 = new TalonSRX(RobotMap.MOTOR_RIGHT_1_ID);
+ private TalonSRX motorRight2 = new TalonSRX(RobotMap.MOTOR_RIGHT_2_ID);
   /**
    * Creates a new DriveCode.
    */
@@ -21,4 +28,14 @@ public class DriveCode extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+public void setLeftMotors(double speed) {
+motorLeft1.set(ControlMode.PercentOutput, -speed);
+motorLeft2.set(ControlMode.PercentOutput, -speed);
+}
+public void setRightMotors(double speed) {
+  motorRight1.set(ControlMode.PercentOutput, speed);
+  motorRight2.set(ControlMode.PercentOutput, speed);
+  }
+
 }
